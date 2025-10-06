9615f79701495717bfcc4fe0bc7c0d5fd30ae42d
jdobmej0511 <jdobmej0511@g.educaand.es>
2025-10-06 10:28:16 +0200

    comit

--- tema_02/variables.java
+++ tema_02/variables.java
@@ -2,10 +2,35 @@ public class variables {
     
     public static void main(String[] args) {
         
-        int numero ;
-        numero=5 ;
+        double x ;
+        double y ;
+        int numero = 5;
+        
 
         System.out.printf("El valor de la variable es: %d", numero);
-       
+        System.out.println("El valor de la variable es:" + numero);
+
+        x = 7 ;
+        y = 25.75 ;
+
+//Ejemplo 2
+        System.out.println("El valor de la variable" + x + "y el valor de y es" + y );
+        System.out.printf( "El valor de x es %.2f y el vlaor de y es %.2f\n", x , y);
+
+
+        //Ejemplo 3
+        char letra1 = 'c' ;
+        char letra2 = 'a' ;
+        char letra3 ='s' ;
+        char letra4 = 'a' ;
+
+        System.out.println(letra1);
+        System.out.println("La primera letra es: " + letra1);
+        System.out.printf("la primera letra es: %c\n", letra1);
+
+        //Ejemplo 4
+        numero++ ;
+        System.out.printf("El valor de la variable es: %d\n", numero++);
+        System.out.printf("El valor de la variable es: %d\n", --numero);
     }
 }
